74e78efa379e6ccfe61852392a3c94004276f2e6
rainx-10 <rainxgomes@gmail.com>
2025-03-29 14:40:46 +0000

    dark and light mode toggle

--- src/app/pokemon/pokemon.component.ts
+++ src/app/pokemon/pokemon.component.ts
@@ -10,6 +10,7 @@ interface Pokemon {
   base_experience: number;
   height: number;
   weight: number;
+  stats: { base_stat: string, stat: { name: string} } [];
 }
 
 @Component({
@@ -18,12 +19,29 @@ interface Pokemon {
   styleUrls: ['./pokemon.component.css']
 })
 export class PokemonComponent implements OnInit {
+
+  isDarkMode = false; 
+  searchQuery: string = '';
   pokemonList: Pokemon[] = [];
-  displayedColumns: string[] = ['Name', 'Image', 'Abilities', 'Types', 'Base Experience', 'Height', 'Weight'];
+  filteredPokemon = this.pokemonList;
+  displayedColumns: string[] = [
+    '#',
+    'Name',
+    'Sprite',
+    'Type',
+    'Total',
+    'HP',
+    'Attack',
+    'Defense',
+    'Sp.Atk',
+    'Sp.Def',
+    'Speed'
+  ];
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.loadDarkModePreference();
     this.fetchPokemonData();
   }
 
@@ -39,5 +57,32 @@ export class PokemonComponent implements OnInit {
         }
       );
     });
+  }
+
+  // Filter Pokémon based on the search query
+  filterPokemon() {
+    if (this.searchQuery.trim() === '') {
+      this.filteredPokemon = this.pokemonList; // If no query, show all Pokémon
+    } else {
+      this.filteredPokemon = this.pokemonList.filter(pokemon =>
+        pokemon.name.toLowerCase().includes(this.searchQuery.toLowerCase())
+      );
+    }
+  }
+
+  getTotalStats(stats: any[]): number {
+    return stats.reduce((sum, stat) => sum + stat.base_stat, 0);
+  }
+
+  toggleDarkMode(): void {
+    this.isDarkMode = !this.isDarkMode;
+    document.body.classList.toggle('dark-mode', this.isDarkMode);
+    localStorage.setItem('darkMode', this.isDarkMode.toString());
+  }
+
+  loadDarkModePreference(): void {
+    const isDarkMode = localStorage.getItem('darkMode') === 'true';
+    this.isDarkMode = isDarkMode;
+    document.body.classList.toggle('dark-mode', isDarkMode);
   }
 }
